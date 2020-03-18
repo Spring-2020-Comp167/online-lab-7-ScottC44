204bcc3b520e96aa73f6d9685d446429e62ff57a
ScottC44 <slcromling@aggies.ncat.edu>
2020-03-18 10:19:45 -0400

    created textbook class and created objects in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
-        Instructor ins1=new Instructor("Scott","Cromling","Cherry",214);
-        Instructor ins2=new Instructor();
+
+        Instructor ins1 = new Instructor("Scott","Cromling","Cherry",214);
+        Instructor ins2 = new Instructor();
+        Textbook tb1 = new Textbook("I hate java","this shit sucks",5);
+        Textbook tb2 = new Textbook("i just want","to graduate",11);
+        System.out.println(tb1);
+        System.out.println(tb2);
         System.out.println(ins1);
         System.out.println(ins2);
     }
---
+++ src/Textbook.java
@@ -0,0 +1,50 @@
+public class Textbook {
+    private String title;
+    private String publisher;
+    private int edition;
+
+    public Textbook() {
+        this.title = "";
+        this.publisher = "";
+        this.edition = 0;
+    }
+
+
+    public Textbook(String title, String publisher, int edition) {
+        this.title = title;
+        this.publisher = publisher;
+        this.edition = edition;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+
+    public void setEdition(int edition) {
+        this.edition = edition;
+    }
+
+
+    @Override
+    public String toString() {
+        return title + System.lineSeparator()+ publisher
+                +System.lineSeparator()+ "" + edition;
+    }
+}
