12edd9775a3f46b1377ad8e0eacbb212c6628e4e
ScottC44 <slcromling@aggies.ncat.edu>
2020-03-18 10:15:31 -0400

    fixed level 1 and added main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Instructor ins1=new Instructor("Scott","Cromling","Cherry",214);
+        Instructor ins2=new Instructor();
+        System.out.println(ins1);
+        System.out.println(ins2);
+    }
+}
